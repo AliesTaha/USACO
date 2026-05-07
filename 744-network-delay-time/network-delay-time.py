691307db4f5e65767c0212695f0e941b1dbab5af
ali taha <aliestaha04@gmail.com>
2026-05-06 17:25:50 -0700

    Time: 372 ms (11.49%) | Memory: 21.6 MB (19.27%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.py
@@ -0,0 +1,31 @@
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        adjacency_list=[[] for _ in range(n+1)]
+        for u, v, w in times:
+            adjacency_list[u].append((v,w))
+        print(adjacency_list)
+
+        INF = float('inf')
+        dist = [INF for _ in range(n+1)]
+        dist[k]=0
+        min_heap= [(0, k)]
+        visited=set()
+
+        while min_heap:
+            curr_d, node=heapq.heappop(min_heap)
+            if node in visited:
+                continue
+            neighbors=adjacency_list[node]
+            visited.add(node)
+            for v,w in neighbors:
+                new_dist= curr_d+w
+                if new_dist < dist[v]:
+                    dist[v]=new_dist
+                final_d=dist[v]
+                if v not in visited:
+                    heapq.heappush(min_heap, (final_d, v))
+        
+        if INF in dist[1:]:
+            return -1
+
+        return max(dist[1:])
