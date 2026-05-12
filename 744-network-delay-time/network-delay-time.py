bfb96819c9d020fb23bcc176e01d3d6bd0eea049
ali taha <aliestaha04@gmail.com>
2026-05-12 13:55:12 -0700

    Time: 345 ms (44.18%) | Memory: 21.1 MB (77.90%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,29 +1,25 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        adjacency_list=[[] for _ in range(n+1)]
-        for u, v, w in times:
-            adjacency_list[u].append((v,w))
-                  
-        INF = float('inf')
-        dist = [INF for _ in range(n+1)]
-        dist[k]=0
-        min_heap= [(0, k)]
+        
+        dic = defaultdict(list)
+        
+        for n1, n2, w in times:
+            dic[n1].append((n2, w))
+        
+        heap=[]
+        heapq.heappush(heap, (0, k))
         visited=set()
-
-        while min_heap:
-            curr_d, node=heapq.heappop(min_heap)
+        t_final=0
+        while heap:
+            t, node=heapq.heappop(heap)
             if node in visited:
                 continue
-            neighbors=adjacency_list[node]
+            t_final=max(t, t_final)
             visited.add(node)
-            for v,w in neighbors:
-                new_dist= curr_d+w
-                if new_dist < dist[v]:
-                    dist[v]=new_dist
-                final_d=dist[v]
-                heapq.heappush(min_heap, (final_d, v))
-        
-        if INF in dist[1:]:
-            return -1
+            for neighbor, t2 in dic[node]:
+                total_time=t+t2
+                heapq.heappush(heap, (total_time, neighbor))
 
-        return max(dist[1:])
+        if len(visited)!=n:
+            return -1
+        return t_final
